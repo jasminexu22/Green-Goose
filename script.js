523fea90039fca8e2d356aadea41fbd3f572536e
Jasmine Xu <xujasmine2@gmail.com>
2024-03-16 13:04:56 -0400

    add sidebar, add more markers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 43.47341501873188, lng: -80.53940053911185 },
-        zoom: 18,
+        center: { lat: 43.47283057937074, lng: -80.54033956546421 },
+        zoom: 19,
         mapId: '26da08bbc3d6e826'
     });
 
@@ -13,20 +13,106 @@ function initMap() {
 
     const markers = [
         {
-            name: "random trash can #1",
-            latitude: 43.473039290458125,
-            longitude: -80.53976273955261,
+            name: "Merci beacoup!",
+            latitude: 43.4730130,
+            longitude: -80.5395363,
             content: "i loooove cats"
             // icon: xxx url
         },
 
         {
-            name: "random can #2",
-            latitude: 43.473267817842036,
-            longitude: -80.54115614006751,
+            name: "#2",
+            latitude: 43.47312665774592,
+            longitude: -80.5390576852732,
+            content: "i loooove dogs"
+            /// icon: xxx url
+            // 43.47312665774592, -80.5390576852732
+        },
+
+        {
+            name: "#3",
+            latitude: 43.47483437092914,
+            longitude: -80.54135692885755,
             content: "i loooove dogs"
             /// icon: xxx url
         },
+
+        //43.474198371489244, -80.54010161022063
+
+        {
+            name: "#4",
+            latitude: 43.474198371489244,
+            longitude: -80.54010161022063,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+
+        //43.47298339972288, -80.539591255965
+
+        {
+            name: "#5",
+            latitude: 43.47298339972288,
+            longitude: -80.539591255965,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+
+        // 43.472973000600355, -80.54015363018016
+
+        {
+            name: "#6",
+            latitude: 43.472973000600355,
+            longitude: -80.54015363018016,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+
+        // 43.47269537267563, -80.53988161233684
+
+        {
+            name: "#7",
+            latitude: 43.47269537267563,
+            longitude: -80.53988161233684,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+
+        // 43.473442387113245, -80.54130302537226
+
+        {
+            name: "#8",
+            latitude: 43.473442387113245,
+            longitude: -80.54130302537226,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+
+        {
+            name: "#9",
+            latitude: 43.473442387113245,
+            longitude: -80.54130302537226,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+
+        /// 43.473123367922575, -80.54026769648893
+
+        {
+            name: "#10",
+            latitude: 43.473123367922575,
+            longitude: -80.54130302537226,
+            content: "i loooove dogs"
+            /// icon: xxx url
+        },
+        
+
+
+
+
+
+
+
+
 
         // add more as needed (just copy paste, change data)
     ];
