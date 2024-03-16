b419f5e3193c27e7d074ad13152286e18aa8fb5f
Jasmine Xu <xujasmine2@gmail.com>
2024-03-15 20:45:48 -0400

    first commit!

---
+++ script.js
@@ -0,0 +1,11 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: {
+            lat: 43.47341501873188,
+            lng: -80.53940053911185
+          }
+          ,
+        zoom: 18,
+        mapid: '26da08bbc3d6e826'
+    });
+}
